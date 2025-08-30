c3e83a2070233fc5ca3c1da4b2dd1762a3629db8
KyvenT <kyventran@gmail.com>
2025-08-29 22:08:23 -0400

    restructured mutations hook and usage

--- frontend/src/components/InboxButton.tsx
+++ frontend/src/components/InboxButton.tsx
@@ -1,52 +1,65 @@
-import React, { useState } from "react";
+import React from "react";
 import useAuthContext from "../hooks/useAuthContext";
 import { useCustomQuery } from "../hooks/useCustomQuery";
 import type { Invite, InviteResponse } from "../types/Invite";
 import DropdownButton from "./DropdownButton";
-import { useCustomMutation } from "../hooks/useCustomMutation";
+import { mutationFunction, type MutationArgs } from "../hooks/useCustomMutation";
+import { useMutation } from "@tanstack/react-query";
 
 const InboxButton = () => {
-    const [inviteRespondedTo, setInviteRespondedTo] = useState<string | undefined>(undefined);
-    const [isAccepted, setIsAccepted] = useState<boolean | undefined>(undefined);
-    const { user } = useAuthContext();
-    const { data: invitesData } = useCustomQuery<Invite>(["inbox", user.token], "invite");
-    const mutation = useCustomMutation<InviteResponse>(
-        [isAccepted ? "accepted" : "rejected", 
-            inviteRespondedTo ?? ""], 
-        "invite-response", {inviteId: inviteRespondedTo, inviteAccepted: isAccepted}); 
+  const { user } = useAuthContext();
+  const { data: invitesData } = useCustomQuery<Invite>(
+    ["inbox", user.token],
+    "invite",
+  );
+  const mutation = useMutation<InviteResponse, Error, MutationArgs>({
+      mutationFn: mutationFunction<InviteResponse>});
 
-    const handleInviteResponse = (event: React.FormEvent, userAccepted: boolean) => {
-        event.preventDefault();
-        setIsAccepted(userAccepted);
-        setInviteRespondedTo((event.target as HTMLButtonElement).form?.id);
-        mutation.mutate();
-        const responseData = mutation.data;
-
-        if (userAccepted) {
-            console.log("Invite accepted");
-            console.log(responseData);
-        } else {
-            console.log("Invite rejected");
-            console.log(responseData);
-        }
+  const handleInviteResponse = (
+    event: React.FormEvent,
+    userAccepted: boolean,
+  ) => {
+    event.preventDefault();
+    const inviteId = (event.target as HTMLButtonElement).form?.id;
+      
+    if (userAccepted) {
+      console.log("Invite accepted");
+      mutation.mutate({fetchUrl: "http://localhost:3000/api/invite/accept", user, method: "PATCH", reqBody: {inviteId}});
+    } else {
+      console.log("Invite rejected");
+      mutation.mutate({fetchUrl: "http://localhost:3000/api/invite/delete", user, method: "DELETE", reqBody: {inviteId}});
     }
+    const responseData = mutation.data;
+    console.log(responseData);
+  };
 
-    return (
-        <DropdownButton buttonText="Inbox">
-            <ul>
-                {invitesData && invitesData.map((invite) => {
-                    return <li key={invite.id}>
-                        <h5>{invite.chatroom.title}</h5>
-                        <p>Invited by: {invite.sender.username}</p>
-                        <form id={invite.id}>
-                            <button onClick={(event) => handleInviteResponse(event, true)}>Accept</button>
-                            <button onClick={(event) => handleInviteResponse(event, false)}>Reject</button>
-                        </form>
-                    </li>
-                })}
-            </ul>
-        </DropdownButton>
-    )
-}
+  return (
+    <DropdownButton buttonText="Inbox">
+      <ul>
+        {invitesData &&
+          invitesData.map((invite) => {
+            return (
+              <li key={invite.id}>
+                <h5>{invite.chatroom.title}</h5>
+                <p>Invited by: {invite.sender.username}</p>
+                <form id={invite.id}>
+                  <button
+                    onClick={(event) => handleInviteResponse(event, true)}
+                  >
+                    Accept
+                  </button>
+                  <button
+                    onClick={(event) => handleInviteResponse(event, false)}
+                  >
+                    Reject
+                  </button>
+                </form>
+              </li>
+            );
+          })}
+      </ul>
+    </DropdownButton>
+  );
+};
 
 export default InboxButton;
--- frontend/src/hooks/useCustomMutation.ts
+++ frontend/src/hooks/useCustomMutation.ts
@@ -1,5 +1,4 @@
 import { useMutation } from "@tanstack/react-query";
-import useAuthContext from "./useAuthContext";
 import type { UserAuth } from "../types/User";
 
 export const MutationResourceType = {
@@ -7,13 +6,23 @@ export const MutationResourceType = {
   CREATE_CHATROOM: "create-chatroom",
 } as const;
 
-const fetchWithBody = (
-  fetchUrl: string,
-  method: string,
-  user: UserAuth,
-  reqBody: {},
-) => {
-  return fetch(fetchUrl, {
+export interface MutationArgs {
+  fetchUrl: string;
+  method: string;
+  user: UserAuth;
+  reqBody: {};
+};
+
+export const mutationFunction = async <T>({
+  fetchUrl,
+  method,
+  user,
+  reqBody,
+}: MutationArgs) => {
+  if (!user.userId) {
+    throw new Error("not authenticated");
+  }
+  const res = await fetch(fetchUrl, {
     method,
     headers: {
       "Content-Type": "application/json",
@@ -21,76 +30,25 @@ const fetchWithBody = (
     },
     body: JSON.stringify(reqBody),
   });
+
+  if (!res.ok) {
+    throw new Error("mutation error");
+  }
+
+  return await res.json() as Promise<T>;
 };
 
 const query = <T>(
   fetchUrl: string,
   method: string,
   user: UserAuth,
-  isLoggedIn: boolean,
   body: {} = {},
   queryKeys: string[],
   queryOptions?: {},
 ) => {
   return useMutation({
     mutationKey: ["customQuery", ...queryKeys],
-    mutationFn: async () => {
-      if (!isLoggedIn) {
-        return [];
-      }
-      let res = await fetchWithBody(fetchUrl, method, user, body);
-
-      if (!res.ok) {
-        console.error(res);
-        return [];
-      }
-      return (await res.json()) as T[];
-    },
+    mutationFn: () => mutationFunction<T>({fetchUrl, method, user, reqBody: body}),
     ...queryOptions,
   });
-};
-
-export const useCustomMutation = <T>(
-  queryKeys: string[],
-  resource: string,
-  options: {
-    inviteId?: string;
-    inviteAccepted?: boolean;
-    queryOptions?: {};
-  } = {},
-) => {
-  const { isLoggedIn, user } = useAuthContext();
-  const { inviteId, inviteAccepted, queryOptions } = options;
-
-  let method = "";
-  let reqBody: any = {};
-  let fetchUrl = "";
-  console.log("mutating " + resource);
-  switch (resource) {
-    // respond to an invite (accept or reject)
-    case MutationResourceType.INVITE_RESPONSE:
-      if (!inviteAccepted || !inviteId) {
-        console.error("need id and response to respond to invites");
-      }
-      method = inviteAccepted ? "PATCH" : "DELETE";
-      fetchUrl = inviteAccepted
-        ? "http://localhost:3000/api/invite/accept"
-        : "http://localhost:3000/api/invite/delete";
-      reqBody = { inviteId };
-      break;
-    case MutationResourceType.CREATE_CHATROOM:
-      break;
-    default:
-      throw new Error("Invalid resource type");
-  }
-
-  return query<T>(
-    fetchUrl,
-    method,
-    user,
-    isLoggedIn,
-    reqBody,
-    queryKeys,
-    queryOptions,
-  );
 };
