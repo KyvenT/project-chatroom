e5701448448b589ce6e8de8a1789e45c91779c4b
KyvenT <kyventran@gmail.com>
2026-01-28 20:21:42 -0500

    add rate limiting to ws messages

--- backend/src/middleware/rateLimitMiddleware.ts
+++ backend/src/middleware/rateLimitMiddleware.ts
@@ -1,42 +1,64 @@
 import { NextFunction, Request, Response } from "express";
 
-const requests = new Map<string, { count: number; timeOfLastRequest: Date }>();
-const limitPerInterval = 20;
-const timeInterval = 60 * 1000; // 60 * 1000 = 60sec
+export type RateLimitResult = { ok: true } | { ok: false };
+export type RateLimitWindowCount = { count: number; windowStart: Date };
+
+const httpRequests = new Map<string, RateLimitWindowCount>();
+const LIMIT_PER_INTERVAL = 60;
+const TIME_INTERVAL = 60 * 1000; // 60 * 1000 = 60sec
 
 export const rateLimitMiddleware = (
   req: Request,
   res: Response,
-  next: NextFunction
+  next: NextFunction,
 ) => {
   if (!req.ip) {
     res.status(500).json({ message: "Request missing ip" });
     return;
   }
 
-  let requestCount = requests.get(req.ip);
+  const result = rateLimit(
+    req.ip,
+    httpRequests,
+    LIMIT_PER_INTERVAL,
+    TIME_INTERVAL,
+  );
 
-  if (!requestCount) {
-    requestCount = { count: 1, timeOfLastRequest: new Date() };
-  } else {
-    if (
-      new Date().getTime() - requestCount.timeOfLastRequest.getTime() <=
-      timeInterval
-    ) {
-      requestCount = {
-        count: requestCount.count + 1,
-        timeOfLastRequest: new Date(),
-      };
-    } else {
-      requestCount = { count: 1, timeOfLastRequest: new Date() };
-    }
-  }
-
-  if (requestCount.count > limitPerInterval) {
-    res.status(500).json({ message: "Too many requests" });
+  if (!result.ok) {
+    res.status(429).json({ message: "Too many requests" });
     return;
   }
 
-  requests.set(req.ip, requestCount);
   next();
+};
+
+export const rateLimit = (
+  identifier: string,
+  requests: Map<string, RateLimitWindowCount>,
+  limit: number,
+  interval: number,
+): RateLimitResult => {
+  const now = new Date();
+  let requestCount = requests.get(identifier);
+
+  if (
+    !requestCount ||
+    now.getTime() - requestCount.windowStart.getTime() > interval
+  ) {
+    requestCount = { count: 1, windowStart: new Date() };
+    requests.set(identifier, requestCount);
+    return { ok: true };
+  }
+
+  if (requestCount.count >= limit) {
+    return { ok: false };
+  }
+
+  requestCount = {
+    count: requestCount.count + 1,
+    windowStart: requestCount.windowStart,
+  };
+
+  requests.set(identifier, requestCount);
+  return { ok: true };
 };
--- backend/src/wss/wss.ts
+++ backend/src/wss/wss.ts
@@ -4,6 +4,16 @@ import { IncomingMessage, Server, ServerResponse } from "http";
 import { wsMessageRouter } from "./router.js";
 import { WSMessageSchema } from "../validators/ws/wsValidation.js";
 import { validate } from "../validators/validate.js";
+import {
+  rateLimit,
+  RateLimitWindowCount,
+} from "../middleware/rateLimitMiddleware.js";
+import { WSMessageTypes } from "../types/ws-messages.js";
+
+const messageCount = new Map<string, RateLimitWindowCount>();
+
+const WS_MESSAGE_LIMIT = 20;
+const WS_MESSAGE_INTERVAL = 1000; // 1000 = 1sec
 
 export const startWSS = (
   server: Server<typeof IncomingMessage, typeof ServerResponse>,
@@ -14,17 +24,44 @@ export const startWSS = (
     console.log("New client connected");
 
     ws.on("message", (data: string) => {
-      const message = JSON.parse(data);
-      const result = validate(WSMessageSchema, message);
+      let message;
+      try {
+        message = JSON.parse(data);
+      } catch (err) {
+        console.error("invalid json format");
+        return;
+      }
 
-      if (!result.ok) {
+      const validated = validate(WSMessageSchema, message);
+      const userId = socketMap.getByValue(ws);
+
+      if (!validated.ok) {
         console.log(message);
-        console.error("ws message validation error", result.error);
+        console.error("ws message validation error", validated.error);
+        return;
+      }
+
+      if (userId) {
+        const rateLimited = rateLimit(
+          userId,
+          messageCount,
+          WS_MESSAGE_LIMIT,
+          WS_MESSAGE_INTERVAL,
+        );
+
+        if (!rateLimited.ok) {
+          console.error("ws message rate limit reached for ", userId);
+          return;
+        }
+      }
+
+      if (!userId && validated.data.type !== WSMessageTypes.Auth) {
+        console.error("unauthorized ws message");
         return;
       }
 
       console.log(`Received websocket message type: ${message.type}`);
-      wsMessageRouter(result.data, ws);
+      wsMessageRouter(validated.data, ws);
     });
 
     ws.on("close", () => {
