7cf944f401598199ee963f8747106d624f8354a2
KyvenT <kyventran@gmail.com>
2026-02-21 02:16:34 -0500

    fix ws url

--- frontend/src/env.ts
+++ frontend/src/env.ts
@@ -1,4 +1,4 @@
 //export const API_URL = "http://localhost:3000";
 export const API_URL = "";
 //export const WS_URL = `ws://localhost:3000`;
-export const WS_URL = `ws://${window.location.host}`;
+export const WS_URL = `wss://${window.location.host}`;
